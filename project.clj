25202b3960843823c002565ddc75d0e7654004a8
SevereOverfl0w <overfl0w@narna.co>
2015-09-02 02:37:51 +0100

    add codox.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]]
+  :plugins [[codox "0.8.13"]])
